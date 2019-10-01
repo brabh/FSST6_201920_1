843db48338827f8cea43effec8156de6daa4230f
Mag Hans Brabenetz <hans@brabenetz.com>
2019-10-01 15:18:39 +0200

    Our first C++ file

---
+++ Quelle.cpp
@@ -0,0 +1,42 @@
+/* */
+
+//
+//
+//
+//
+//
+
+//
+
+#include <iostream>
+#include <cstdio>
+#include "Header.h"
+
+using namespace std;
+using std::cout;
+
+namespace m {
+	int x;
+	double d;
+	char c;
+
+}
+
+
+
+int main() {
+
+	std::cout << "Hello my friend" << std::endl;
+	cout << "we have a nice day";
+
+	std::printf("and another hello");
+	printf("irgendwas");
+
+	m::x = 7;
+
+	f1();
+
+	//std::getchar();
+	return 0;
+}
+
