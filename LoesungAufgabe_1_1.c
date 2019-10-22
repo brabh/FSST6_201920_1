36e2e145d04d14bcdb24e7bb9418d4bbc5f0effc
Mag Hans Brabenetz <hans@brabenetz.com>
2019-10-22 15:11:40 +0200

    Loesung Aufgabe 1.1

---
+++ LoesungAufgabe_1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello von Aufgabe 1.1\n");
+
+	int a = 1, b = 2, c = 3;
+	int sum;
+
+	sum = a + b + c;
+
+	printf("Sum = %d\n", sum);
+
+	getchar();
+	return 0;
+}
+
