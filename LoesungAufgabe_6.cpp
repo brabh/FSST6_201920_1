8b0409ab57b265f7acdc4031208605bc2bc72e8d
Mag Hans Brabenetz <hans@brabenetz.com>
2019-11-19 13:51:09 +0100

    good

--- LoesungAufgabe_6.cpp
+++ LoesungAufgabe_6.cpp
@@ -28,6 +28,33 @@ int L6main() {
 		cout << "My binary a is " << a << endl;
 	fib.close();
 
+	
+	
+	//
+	// or Measure the file first
+	//
+	std::ifstream fix("MyInts.bin", std::ios::ate | std::ios::binary); // set filepointer to end of file
+
+	int fileLength = fix.tellg(); //  get filesize
+	int N_ints = fileLength / sizeof(int); // calc amount of int in file
+
+	cout << "fileLength " << fileLength << endl;
+	cout << "N_ints " << N_ints << endl;
+
+	int* pi = new int[N_ints]; // request memory space for int's
+
+	for (int i = 0; i < N_ints; ++i)
+		pi[i] = -1;
+
+	fix.seekg(0, std::ios::beg); // reset filepointer to beginning of file
+
+	fix.read(reinterpret_cast<char*>(pi), sizeof(int)* N_ints); // read ints into heap memory
+
+	fix.close();
+
+	for (int i = 0; i < N_ints; ++i)
+		cout << "cout pi[] = " << pi[i] << endl;
+
 	getchar();
 	return 0;
 }
