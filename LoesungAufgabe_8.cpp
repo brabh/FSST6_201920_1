796f8a3ac11b48d7f151f93eac693e31435e1cb8
Mag Hans Brabenetz <hans@brabenetz.com>
2019-12-03 15:20:28 +0100

    Loesung  Aufgabe 8

--- LoesungAufgabe_8.cpp
+++ LoesungAufgabe_8.cpp
@@ -2,7 +2,8 @@
 
 /* Lösung Aufgabe 8:
 ** MFC Dialog Application mttels MFC Wizzard erstellen
-** In der Resourcenansicht zwei Textfelder und einen Button in den Dialog hinzufügen 
+** In der Resourcenansicht aus der Toolbox zwei Textfelder (Edit Control) 
+** und einen Button in den Dialog hinzufügen 
 ** Für die Textfelder je eine Variable hinzufügen
 ** Für den Button das Event OnBnClicked hinzufügen
 ** die gewünshcte Funktionalität in diese neue Funktion schreiben:
