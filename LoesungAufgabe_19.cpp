2d51769ca222e2fbc5f97e469b498760b13538f1
Mag Hans Brabenetz <hans@brabenetz.com>
2020-02-24 18:16:44 +0100

    Loesung fehlt noch

--- LoesungAufgabe_19.cpp
+++ LoesungAufgabe_19.cpp
@@ -17,24 +17,3 @@ double value;
 };
 */
 
-#include <iostream>
-#include <functional>
-
-int  f1(int x, std::function<int(int)> fct) {
-	
-	return fct(x);
-}
-
-int main() {
-	int loc = 100, loc2 = 200;
-
-	auto fx = [&loc, &loc2](int x) {return 7 + x + loc; };
-
-	std::cout << f1(loc, fx) << std::endl;
-	std::cout << f1(loc, [=](int x) {return 7 + x + loc; }) << std::endl;
-
-	auto f = std::bind(f1, std::placeholders::_1, fx);
-	std::cout << f(25) << std::endl;
-
-	return 0;
-}
